c1e06a0e256fcbf11a7507fc7377448b94d86880
Allejandro <allejandro.empresarial@gmail.com>
2024-01-15 11:43:04 -0300

    Adding a path to add new topics and entries

--- learning_curves/views.py
+++ learning_curves/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The initial page of Learning Curve."""
@@ -18,3 +21,33 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_curves/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_curves:topics'))
+        
+    context = {'form': form}
+    return render(request, 'learning_curves/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry to a specific topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_curves:topic', args=[topic_id]))
+        
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_curves/new_entry.html', context)
