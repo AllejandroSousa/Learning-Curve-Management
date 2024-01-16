1e1907a3faf1a2f1d57c5d61c2519b6e3d52a404
Allejandro <allejandro.empresarial@gmail.com>
2024-01-16 12:13:43 -0300

    Restricting subjects only to their respective users and safeguarding pages against attacks

--- learning_curve/settings.py
+++ learning_curve/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #My applications
     'learning_curves',
     'users',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#My settings
+LOGIN_URL = '/users/login/'
--- learning_curves/views.py
+++ learning_curves/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,19 +10,24 @@ def index(request):
     """The initial page of Learning Curve."""
     return render(request, 'learning_curves/index.html')
 
+@login_required
 def topics(request):
     """Show all the topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_curves/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and its all entries."""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_curves/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -29,12 +35,15 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_curves:topics'))
         
     context = {'form': form}
     return render(request, 'learning_curves/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry to a specific topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -44,6 +53,8 @@ def new_entry(request, topic_id):
     else:
         form = EntryForm(data=request.POST)
         if form.is_valid():
+            if topic.owner != request.user:
+                raise Http404
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
@@ -52,11 +63,14 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_curves/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-
+    if topic.owner != request.user:
+        raise Http404
+    
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
